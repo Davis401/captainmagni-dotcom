13b717022ebdaff2a6bd24a45837331fbaaf4744
Davis401 <davisjohnson401@gmail.com>
2023-07-23 10:17:13 -0400

    Re commit to rebuild

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://captainmagni.com/',
+  site: 'https://www.captainmagni.com/',
   integrations: [tailwind()]
 });
